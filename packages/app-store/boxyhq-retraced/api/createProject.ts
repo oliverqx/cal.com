74bf5a0054240968754a136b8e4482a723bf833e
oliver <8559757+oliverqx@users.noreply.github.com>
2024-06-03 10:47:31 -0700

    feat: add createDefaultInstallation to createProject endpoint
    In preparation for fully managed BoxyHQ experience

--- packages/app-store/boxyhq-retraced/api/createProject.ts
+++ packages/app-store/boxyhq-retraced/api/createProject.ts
@@ -1,10 +1,14 @@
 import type { NextApiRequest, NextApiResponse } from "next";
 import z from "zod";
 
+import getInstalledAppPath from "@calcom/app-store/_utils/getInstalledAppPath";
+import { createDefaultInstallation } from "@calcom/app-store/_utils/installation";
+import { HttpError } from "@calcom/lib/http-error";
 import { defaultResponder } from "@calcom/lib/server";
 
+import appConfig from "../config.json";
+import type { BoxyHqProject } from "../lib/boxysdk";
 import { boxyHQAuthenticate, createProject } from "../lib/boxysdk";
-import { getIdentifier } from "../lib/utils";
 
 const ZPingInputSchema = z.object({
   sudoKey: z.string(),
@@ -13,15 +17,59 @@ const ZPingInputSchema = z.object({
 });
 
 export async function handler(req: NextApiRequest, res: NextApiResponse) {
-  const email = getIdentifier(req);
-  const { sudoKey, boxyHqEndpoint, projectName } = ZPingInputSchema.parse(req.body);
-  const boxyAuthenticationToken = await boxyHQAuthenticate(sudoKey, boxyHqEndpoint, email);
+  const session = req.session;
+  if (!session || !session.user.email) {
+    throw new HttpError({ statusCode: 401, message: "Unauthorized. User is missing email." });
+  }
 
+  const { sudoKey, boxyHqEndpoint, projectName } = ZPingInputSchema.parse(req.body);
+  const boxyAuthenticationToken = await boxyHQAuthenticate(sudoKey, boxyHqEndpoint, session.user.email);
+
+  let projectMetadata: BoxyHqProject | undefined;
   try {
-    await createProject(boxyAuthenticationToken, projectName, boxyHqEndpoint);
-    return res.status(200).json({ message: "Templates created successfully." });
+    projectMetadata = await createProject(boxyAuthenticationToken, projectName, boxyHqEndpoint);
   } catch (e) {
     return res.status(500).json({ message: e });
+  }
+
+  const environments: { [key: string]: { id: string; name: string; token: string } } = {};
+
+  projectMetadata?.environments.forEach((environment) => {
+    const token = projectMetadata?.tokens.find((token) => token.environment_id === environment.id)?.token;
+    environments[environment.id] = {
+      id: environment.id,
+      name: environment.name,
+      token: token as string,
+    };
+  });
+
+  const boxyHqMetadata = {
+    name: projectMetadata?.name,
+    projectId: projectMetadata?.id,
+    environments: environments,
+  };
+
+  try {
+    const appCredential = await createDefaultInstallation({
+      appType: appConfig.type,
+      user: session.user,
+      slug: appConfig.slug,
+      key: {
+        activeEnvironment: Object.values(boxyHqMetadata.environments)[0].id,
+        projectId: boxyHqMetadata.projectId,
+        endpoint: boxyHqEndpoint,
+      },
+      settings: {
+        environments: boxyHqMetadata.environments,
+      },
+    });
+
+    return res.status(200).json({
+      url: getInstalledAppPath({ variant: "auditLogs", slug: appConfig.slug }),
+      credentialId: appCredential.id,
+    });
+  } catch (reason) {
+    return res.status(500).json({ message: "Could not add BoxyHQ Retraced app" });
   }
 }
 
