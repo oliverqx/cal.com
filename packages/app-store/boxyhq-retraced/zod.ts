112a3888b0145df985f12813ffa17e2ace95ab95
oliver <8559757+oliverqx@users.noreply.github.com>
2024-06-03 14:05:17 -0700

    feat: add zod schemas
    atm its dirty

--- packages/app-store/boxyhq-retraced/zod.ts
+++ packages/app-store/boxyhq-retraced/zod.ts
@@ -5,8 +5,15 @@ export const appKeysSchema = z.object({
   projectId: z.string().min(1),
   endpoint: z.string().min(1),
 });
-
 export type AppKeys = z.infer<typeof appKeysSchema>;
+
+export const clientSafeAppKeysSchema = z.object({
+  activeEnvironment: z.object({ label: z.string(), value: z.string(), key: z.string() }),
+  projectId: z.string(),
+  endpoint: z.string(),
+});
+
+export type ClientSafeAppKeysSchema = z.infer<typeof clientSafeAppKeysSchema>;
 
 export const appDataSchema = z.object({});
 
@@ -19,6 +26,53 @@ export const credentialSettingsSchema = z.object({
       token: z.string(),
     })
   ),
+  projectName: z.string(),
 });
 
 export type CredentialSettings = z.infer<typeof credentialSettingsSchema>;
+
+export const ZBoxyProjectCreationInput = z.object({
+  sudoKey: z.string(),
+  boxyHqEndpoint: z.string(),
+  projectName: z.string(),
+});
+export type BoxyProjectCreationInput = z.infer<typeof ZBoxyProjectCreationInput>;
+
+export const boxyEnvironmentTranformInfoClientSafe = z
+  .object({
+    name: z.string(),
+    id: z.string(),
+  })
+  .transform((values) => {
+    return {
+      key: values.id,
+      label: values.name,
+      value: values.name.toLowerCase(),
+    };
+  });
+
+export const boxyEnvironmentClientSafe = z.object({
+  key: z.coerce.string(),
+  label: z.string(),
+  value: z.string(),
+});
+
+export const boxySettingsInfoClientSafe = z.object({
+  projectName: z.string(),
+  environments: z.record(boxyEnvironmentTranformInfoClientSafe),
+});
+
+export const ExpectedCredential = z.object({
+  url: z.string().optional(),
+  activeEnvironment: boxyEnvironmentClientSafe,
+  credentialId: z.number(),
+  endpoint: z.string(),
+  environments: z.array(boxyEnvironmentClientSafe),
+  projectName: z.string(),
+});
+
+export const credentialSettingsFormSchema = z.object({
+  activeEnvironment: z.object({ key: z.string(), label: z.string(), value: z.string() }),
+  projectId: z.string(),
+  endpoint: z.string(),
+});
