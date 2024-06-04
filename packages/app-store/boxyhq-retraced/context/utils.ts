fc2ea1a54120a1b2e95f8a6f98ed0ee70e179503
oliver <8559757+oliverqx@users.noreply.github.com>
2024-06-03 22:20:48 -0700

    feat: create reduccer

---
+++ packages/app-store/boxyhq-retraced/context/utils.ts
@@ -0,0 +1,56 @@
+export enum BoxySetupStages {
+  CREATION = "CREATION",
+  CONFIRMATION = "CONFIRMATION",
+}
+
+export type BoxyHQCredentialState = BoxyHQCredentialConfirm | BoxyHQCredentialCreate;
+
+export type BoxyHQCredentialConfirm = {
+  boxyCredentialState: BoxySetupStages.CONFIRMATION;
+  credentialInfo: {
+    credentialId: number;
+    url: string;
+    options: { label: string; value: string; key: string }[];
+  };
+};
+
+export type BoxyHQCredentialCreate = {
+  boxyCredentialState: BoxySetupStages.CREATION;
+  credentialInfo: undefined;
+};
+
+export type Action = {
+  type: "afterCredentialCreation";
+  payload: BoxyHQCredentialConfirm;
+};
+
+export const reducer = (state: BoxyHQCredentialState, action: Action) => {
+  switch (action.type) {
+    case "afterCredentialCreation":
+      return {
+        ...state,
+      };
+    default:
+      return state;
+  }
+};
+
+// Handlers
+export const afterCredentialCreationSetup = (
+  credentialId: number,
+  url: string,
+  options: { label: string; value: string; key: string }[]
+): Action => {
+  return {
+    type: "afterCredentialCreation",
+    payload: {
+      boxyCredentialState: BoxySetupStages.CONFIRMATION,
+      credentialInfo: { credentialId, url, options },
+    },
+  };
+};
+
+export const initialState = {
+  boxyCredentialState: BoxySetupStages.CREATION as const,
+  credentialInfo: undefined,
+};
