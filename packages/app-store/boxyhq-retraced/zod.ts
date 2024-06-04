728fb918f842f3785c4f2ff7705ce346ec6a4ba4
oliver <8559757+oliverqx@users.noreply.github.com>
2024-06-03 20:00:14 -0700

    feat: clean zod schemas

--- packages/app-store/boxyhq-retraced/zod.ts
+++ packages/app-store/boxyhq-retraced/zod.ts
@@ -1,19 +1,28 @@
 import { z } from "zod";
 
+import { Credential } from "../_utils/zod";
+
 export const appKeysSchema = z.object({
   activeEnvironment: z.string().min(1),
-  projectId: z.string().min(1),
   endpoint: z.string().min(1),
 });
 export type AppKeys = z.infer<typeof appKeysSchema>;
-
-export const appDataSchema = z.object({});
 
 export const boxyHqEnvironmentSchema = z.object({
   id: z.string(),
   name: z.string(),
   token: z.string(),
 });
+
+export const ZBoxyProjectCreationInput = z.object({
+  sudoKey: z.string(),
+  boxyHqEndpoint: z.string(),
+  projectName: z.string(),
+});
+
+export type BoxyProjectCreationInput = z.infer<typeof ZBoxyProjectCreationInput>;
+
+export const appDataSchema = z.object({});
 
 export const appSettingsSchema = z.object({
   disabledEvents: z.array(z.string()),
@@ -23,39 +32,12 @@ export const appSettingsSchema = z.object({
 
 export type AppSettings = z.infer<typeof appSettingsSchema>;
 
-export const ZBoxyProjectCreationInput = z.object({
-  sudoKey: z.string(),
-  boxyHqEndpoint: z.string(),
-  projectName: z.string(),
-});
-export type BoxyProjectCreationInput = z.infer<typeof ZBoxyProjectCreationInput>;
-
-export const boxyEnvironmentTranformInfoClientSafe = boxyHqEnvironmentSchema.transform((values) => {
-  return {
-    key: values.id,
-    label: values.name,
-    value: values.name.toLowerCase(),
-  };
-});
-
-export const boxyEnvironmentClientSafe = z.object({
-  key: z.coerce.string(),
-  label: z.string(),
-  value: z.string(),
-});
-
-export const boxySettingsInfoClientSafe = z.object({
-  projectName: z.string(),
-  environments: z.record(boxyEnvironmentTranformInfoClientSafe),
-});
-
-export const ExpectedCredential = z.object({
-  url: z.string().optional(),
-  activeEnvironment: boxyEnvironmentClientSafe,
-  credentialId: z.number(),
-  endpoint: z.string(),
-  environments: z.array(boxyEnvironmentClientSafe),
-  projectName: z.string(),
+export const getClientSafeAppCredential = Credential.extend({
+  key: appKeysSchema,
+  settings: z.object({
+    projectName: z.string(),
+    environments: z.record(boxyHqEnvironmentSchema.omit({ token: true })),
+  }),
 });
 
 export const appSettingsFormSchema = z.object({
