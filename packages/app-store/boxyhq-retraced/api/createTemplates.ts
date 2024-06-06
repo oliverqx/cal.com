92a9527ca5462ef5b9e388ebe392cb491514e306
oliver <8559757+oliverqx@users.noreply.github.com>
2024-06-06 08:14:47 -0700

    fix: remove getIdentifier

--- packages/app-store/boxyhq-retraced/api/createTemplates.ts
+++ packages/app-store/boxyhq-retraced/api/createTemplates.ts
@@ -1,10 +1,10 @@
 import type { NextApiRequest, NextApiResponse } from "next";
 import z from "zod";
 
+import { HttpError } from "@calcom/lib/http-error";
 import { defaultResponder } from "@calcom/lib/server";
 
-import { boxyHQAuthenticate, boxyHqCreateTemplates, getBoxyHQKey } from "../lib/boxysdk";
-import { getIdentifier } from "../lib/utils";
+import { boxyHQAuthenticate, getBoxyHQKey } from "../lib/boxysdk";
 
 const ZPingInputSchema = z.object({
   credentialId: z.number(),
@@ -13,18 +13,19 @@ const ZPingInputSchema = z.object({
 });
 
 export async function handler(req: NextApiRequest, res: NextApiResponse) {
-  const email = getIdentifier(req);
-
+  if (!req.session?.user?.id || !req.session.user.email) {
+    throw new HttpError({ statusCode: 401, message: "Unauthorized" });
+  }
   const { credentialId, adminRootToken } = ZPingInputSchema.parse(req.body);
   const environmentId = "8a7ce8ffe5aa4dfea050ec54f03c596e";
 
   const boxyHqKey = await getBoxyHQKey(credentialId);
 
   // Admin key is needed to edit templates.
-  const boxyAdminKey = await boxyHQAuthenticate(adminRootToken, boxyHqKey.endpoint, email);
+  const boxyAdminKey = await boxyHQAuthenticate(adminRootToken, boxyHqKey.endpoint, req.session.user.email);
 
   try {
-    await boxyHqCreateTemplates(boxyAdminKey, boxyHqKey.projectId, boxyHqKey.endpoint, environmentId);
+    // await boxyHqCreateTemplates(boxyAdminKey, boxyHqKey.projectId, boxyHqKey.endpoint, environmentId);
     return res.status(200).json({ message: "Templates created successfully." });
   } catch (e) {
     return res.status(500).json({ message: e });
