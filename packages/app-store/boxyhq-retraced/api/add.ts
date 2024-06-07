aba72270b347cbe36116aa6323a3bae445cfa391
oliver <8559757+oliverqx@users.noreply.github.com>
2024-06-07 09:34:07 -0700

    chore: remove _getAdd file

--- packages/app-store/boxyhq-retraced/api/add.ts
+++ packages/app-store/boxyhq-retraced/api/add.ts
@@ -1,5 +1,11 @@
-import { defaultHandler } from "@calcom/lib/server";
+import type { NextApiRequest, NextApiResponse } from "next";
 
-export default defaultHandler({
-  GET: import("./_getAdd"),
-});
+import checkSession from "../../_utils/auth";
+import { checkInstalled } from "../../_utils/installation";
+import config from "../config.json";
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+  const session = checkSession(req);
+  await checkInstalled(config.slug, session.user?.id);
+  return res.status(200).json({ url: `/apps/${config.slug}/setup` });
+}
